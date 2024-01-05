d9393baf9a4932415a3d8182d59afaf1d3b23d69
samojeyinka <samuelojeyinka@gmail.com>
2024-01-04 21:20:59 -1200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import { createContext, useState ,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import About from './pages/about'
-import Home from './pages/Home'
-import Show from './pages/Show'
-import NewNote from './pages/NewNote'
-import Edit from './pages/Edit'
 import FileForm from './components/FileForm'
 import LatestImage from './components/LatestImage'
 import axios from 'axios'
 
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route>
-//       <Route index element={<Home />} />
-//       <Route path="note" element={<Show />} />
-//       <Route path="new" element={<NewNote />} />
-//       <Route path="edit" element={<Edit />} />
-//       <Route path="about" element={<About />} />
-//     </Route>
-//   )
-// )
 
 export const AppContext = createContext(null);
 
@@ -33,7 +13,7 @@ function App() {
 
   const posts = async() => {
     try {
-      const res = await axios.get("http://localhost:3000/api/v1/latest");
+      const res = await axios.get("http://localhost:3000/api/v1/projects");
       console.log(res.data.image_url);
     } catch (error) {
       console.log(error);
--- src/components/FileForm.jsx
+++ src/components/FileForm.jsx
@@ -1,49 +1,52 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../App'
+import axios from 'axios';
 
 function FileForm() {
-    const {latestPost, setLatestPost} = useContext(AppContext)
+    const { setLatestPost } = useContext(AppContext);
   
-    function handleSubmit(event) {
-        event.preventDefault();
-        const data = new FormData();
-
-        data.append("post[title]", event.target.title.value);
-        data.append("post[content]", event.target.content.value);
-        data.append("post[image]", event.target.image.files[0]);
-        submitToAPI(data)
-    }
-
-    function submitToAPI(data) {
-        fetch("http://localhost:3000/api/v1/posts", {
-            method: "POST",
-            body: data,
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            setLatestPost(data.image_url);
-        })
-        .catch((error) => console.error(error));
-    }
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const data = new FormData();
   
+      data.append("project[name]", event.target.title.value);
+      data.append("project[details]", event.target.details.value);
+      data.append("project[description]", event.target.content.value);
+      data.append("project[image]", event.target.image.files[0]);
+  
+      try {
+        const res = await axios.post("http://localhost:3000/api/v1/projects", data, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        setLatestPost(res.data.image_url);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   
     return (
-    <div>
+      <div>
         <h1>File Form</h1>
         <form onSubmit={(e) => handleSubmit(e)}>
-        <label htmlFor="title">Title</label>
-        <input type="text" name='title' id='title' />
-        <br />
-        <label htmlFor="content">Content</label>
-        <input type="text" name='content' id='content' />
-        <br />
-        <label htmlFor="image">Image</label>
-        <input type="file" name='image' id='image' />
-        <br />
-        <button type='submit'>Post</button>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" id="title" />
+          <br />
+          <label htmlFor="content">Description</label>
+          <input type="text" name="content" id="content" />
+          <br />
+          <label htmlFor="details">Details</label>
+          <input type="text" name="details" id="details" />
+          <br />
+          <label htmlFor="image">Image</label>
+          <input type="file" name="image" id="image" />
+          <br />
+          <button type="submit">Post</button>
         </form>
-    </div>
-  )
-}
+      </div>
+    );
+  }
+  
 
 export default FileForm
